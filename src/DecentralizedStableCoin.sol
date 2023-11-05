c1d5e18eb78f5ed3d7cb81c6f2ef29af02045c7e
nikhil <nikhilchary129@gmail.com>
2023-11-05 11:43:27 +0530

    inti

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+pragma solidity ^0.8.19;
+
+contract Counter {
+   
+}
