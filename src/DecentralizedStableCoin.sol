af63bc905d3055b2173610c81565ca6fbf615e1a
nikhil <nikhilchary129@gmail.com>
2023-11-29 23:04:05 +0530

    decentralized stable coin main file 100%

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -21,9 +21,36 @@
 // private
 // view & pure functions
 
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.20;
 
-contract  s{
+import{ ERC20Burnable,ERC20 } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
+abstract  contract  DecentralizedStableCoin is ERC20Burnable,Ownable {
+    error DecentralizedStableCoin__MustBeMoreThanZero();
+    error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizedStableCoin__AmountCantBeSendToZeroAdrress();
+
+    constructor()ERC20("DecentralizedStableCoin","DSC"){} 
+
+    function burn(uint256 _amount)public override onlyOwner{
+        uint256 balance =balanceOf(msg.sender);
+        if(_amount<=0) revert DecentralizedStableCoin__MustBeMoreThanZero();
+        if(balance<_amount)revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+
+        super.burn(_amount);
+    } 
+
+    function mint(address _to,uint256 _amount) external onlyOwner returns (bool){
+        if(_to== address(0)) revert DecentralizedStableCoin__AmountCantBeSendToZeroAdrress();
+        if(_amount<=0) revert DecentralizedStableCoin__MustBeMoreThanZero();
+
+_mint(_to,_amount);
+        return true;
+
+ 
+
+    }
     
    
 }
